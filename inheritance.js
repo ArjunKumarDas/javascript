2373f6294914560f5ee116bbbc606785a47fa8a2
ArjunKumarDas <arjunkdinfo@gmail.com>
2021-07-24 16:22:11 +0600

    another example of inheritance

--- inheritance.js
+++ inheritance.js
@@ -20,3 +20,26 @@ console.log(fristSon);
 console.log(secondSon);
 // console.log(fristSon.getFullName());
 // console.log(secondSon.getFullName());
+
+class shap {
+    constructor(color){
+        this.color
+    }
+    draw(){
+        console.log("Drawing...");
+    }
+}
+
+class Rectangular extends shap{
+    constructor (color, width, height){
+     super(color)   
+     this.width = width;
+     this.height = height;
+    }
+    calculate(){
+        return this.width * this.height;
+    }
+}
+let r = new Rectangular(5, 7)
+console.log(r);
+r.draw();
