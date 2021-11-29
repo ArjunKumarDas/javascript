3a11fdb86fc87c2f3184086c6651defe5cf0eaf8
ArjunKumarDas <arjunkdinfo@gmail.com>
2021-11-29 08:52:43 +0600

    stringsearch

---
+++ JavaScriptStringSearch.js
@@ -0,0 +1,5 @@
+//JavaScript methods for searching strings:
+
+String.indexOf()      //The indexOf() method returns the index of (the position of) the first occurrence of a specified text in a string:
+String.lastIndexOf()  //The search() method searches a string for a specified value and returns the position of the match:
+String.startsWith()   
