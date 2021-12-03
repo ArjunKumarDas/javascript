8b6d589e16fcb2d1859d45ca07f159679423ca8d
ArjunKumarDas <arjunkdinfo@gmail.com>
2021-12-03 23:47:32 +0600

    typeof JS

--- JavaScriptComparisonandLogicalOperators.js
+++ JavaScriptComparisonandLogicalOperators.js
@@ -1,1 +1,25 @@
 // When comparing a string with a number, JavaScript will convert the string to a number when doing the comparison. An empty string converts to 0. A non-numeric string converts to NaN which is always false.
+
+let day 
+switch ( new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
---
+++ JavaScriptTypeof.js
@@ -0,0 +1,14 @@
+// The data type of NaN is number
+// The data type of an array is object
+// The data type of a date is object
+// The data type of null is object
+// The data type of an undefined variable is undefined *
+// The data type of a variable that has not been assigned a value is also undefined *
+
+//             Primitive Data
+// A primitive data value is a single simple data value with no additional properties and methods.
+// The typeof operator can return one of these primitive types:
+// string
+// number
+// boolean
+// undefined
